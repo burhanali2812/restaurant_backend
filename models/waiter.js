87eb9d2424637c42e91cf77534f4b8b4921821b9
burhanali2812 <syedburhanali830@gmail.com>
2026-05-17 01:27:06 +0500

    set timer otp

--- models/waiter.js
+++ models/waiter.js
@@ -1,13 +1,24 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 
-const waiterSchema = new mongoose.Schema({
+const waiterSchema = new mongoose.Schema(
+  {
     name: { type: String, required: true },
-    restaurantId: { type: mongoose.Schema.Types.ObjectId, ref: "Restaurant", required: true },
+    restaurantId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Restaurant",
+      required: true,
+    },
     phone: { type: String, required: true },
     salary: { type: Number, required: true },
-    shift: { type: String, required: true, enum: ["Morning", "Afternoon", "Evening"] },
+    shift: {
+      type: String,
+      required: true,
+      enum: ["Morning", "Afternoon", "Evening"],
+    },
     isActive: { type: Boolean, default: true },
-}, { timestamps: true });
+  },
+  { timestamps: true },
+);
 
 const Waiter = mongoose.model("Waiter", waiterSchema);
-export default Waiter;
+module.exports = Waiter;
